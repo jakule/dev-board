4800a1537d67c7cbd608486d296b6df0e2ab649c
Jakub Nyckowski <nyckowski.jakub@gmail.com>
2023-11-28 22:47:18 -0500

    Apply cargo clippy suggestions

--- src/middleware/cors.rs
+++ src/middleware/cors.rs
@@ -2,10 +2,9 @@ use crate::config::CFG;
 use salvo::cors::{AllowHeaders, AllowMethods, Cors, CorsHandler};
 
 pub fn cors_middleware() -> CorsHandler {
-    let cors_handler = Cors::new()
+    Cors::new()
         .allow_origin(&CFG.server.cors_allow_origin)
         .allow_methods(AllowMethods::any())
         .allow_headers(AllowHeaders::any())
-        .into_handler();
-    cors_handler
+        .into_handler()
 }
