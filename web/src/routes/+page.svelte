3a51485cbc49939bec44a132196ab82e8c8a020e
Jakub Nyckowski <nyckowski.jakub@gmail.com>
2023-11-30 00:31:17 -0500

    Implement API data fetch and style using Bulma CSS framework
    Incorporated API call to fetch PR data from the '/api/prs' endpoint, which gets displayed as a table within the page. Additionally, the Bulma CSS framework has been added for better styling of the page. Essential changes have been made to support these updates across various configuration files including package.json and vite.config.ts.

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,7 +1,12 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let prsResponse: { data: { id: number; title: string; state: string }[] };
+
+	onMount(async function () {
+		const response = await fetch('/api/prs');
+		prsResponse = await response.json();
+	});
 </script>
 
 <svelte:head>
@@ -10,22 +15,26 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<table class="table">
+		<thead>
+			<tr>
+				<th>Id</th>
+				<th>Title</th>
+				<th>State</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#if prsResponse !== undefined}
+				{#each prsResponse.data as pr}
+					<tr>
+						<td>{pr.id}</td>
+						<td>{pr.title}</td>
+						<td>{pr.state}</td>
+					</tr>
+				{/each}
+			{/if}
+		</tbody>
+	</table>
 </section>
 
 <style>
@@ -35,25 +44,5 @@
 		justify-content: center;
 		align-items: center;
 		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,5 +5,10 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		proxy: {
+			'/api': 'http://127.0.0.1:5800'
+		}
 	}
 });
